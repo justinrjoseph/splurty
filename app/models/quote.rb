aa1bf6753219f02b042a6a1b80d5972cef5fe26c
Justin Joseph <justin.joseph0@yahoo.com>
2015-11-18 22:24:44 +0000

    Add unique tag test, method to Quote model, and display on index page

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,11 @@ class Quote < ActiveRecord::Base
   validates :saying, presence: true, length: { minimum: 3, maximum: 140 }
   validates :author, presence: true, length: { minimum: 3, maximum: 50 }
 
+  def unique_tag
+    initials = self.author.split(' ').collect do |initial|
+      initial[0]
+    end
+
+    "#{initials.join}##{id}"
+  end
 end
--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+
+  test "unique tag" do
+    quote = Quote.create!(author: 'Justin Joseph', saying: 'I like beds.')
+    expected_tag = 'JJ#' + quote.id.to_s
+    actual_tag = quote.unique_tag
+    assert_equal expected_tag, actual_tag
   end
+
 end
